bad2ce289df8899302b639f4e0d45cb798207904
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-08-13 18:07:53 -0400

    Implement rudimentary tab logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import home from "./home.js"
+import menu from "./menu.js"
+import contact from "./contact.js"
 
 const container = document.querySelector("#content");
 container.appendChild(home());
+const nav = document.querySelector("nav");
+
+nav.addEventListener('click', event => {
+    if (event.target.tagName === "BUTTON") {
+        container.textContent = "";
+        if (event.target.id === "home") {
+            container.appendChild(home());
+        }
+        else if (event.target.id === "menu") {
+            container.appendChild(menu());
+        } else {
+            container.appendChild(contact());
+        }
+    }
+})
 
