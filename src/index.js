bd40c7743af0c8a1500c3723d4b95e92ccd6aec3
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-08-13 18:30:35 -0400

    Refine and refactor tab logic to work on buttons rather than nav element

--- src/index.js
+++ src/index.js
@@ -3,20 +3,17 @@ import menu from "./menu.js"
 import contact from "./contact.js"
 
 const container = document.querySelector("#content");
-container.appendChild(home());
-const nav = document.querySelector("nav");
-
-nav.addEventListener('click', event => {
-    if (event.target.tagName === "BUTTON") {
+const buttons = document.querySelectorAll("button");
+const pages = {
+    home: home(),
+    menu: menu(),
+    contact: contact(),
+};
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
         container.textContent = "";
-        if (event.target.id === "home") {
-            container.appendChild(home());
-        }
-        else if (event.target.id === "menu") {
-            container.appendChild(menu());
-        } else {
-            container.appendChild(contact());
-        }
-    }
-})
+        container.appendChild(pages[event.target.id]);
+    })
+});
+container.appendChild(pages.home);
 
