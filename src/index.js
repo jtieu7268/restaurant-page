9007e9ee048031b73044ee401c4eb4015d3d94a4
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-08-13 20:49:55 -0400

    Install css loaders, add style sheet, and style navigation bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import home from "./home.js"
 import menu from "./menu.js"
 import contact from "./contact.js"
+import "./style.css"
 
 const container = document.querySelector("#content");
 const buttons = document.querySelectorAll("button");
@@ -13,7 +14,12 @@ buttons.forEach(button => {
     button.addEventListener('click', event => {
         container.textContent = "";
         container.appendChild(pages[event.target.id]);
-    })
+        buttons.forEach(button => {
+            button.classList.remove("clicked");
+        })
+        button.classList.add("clicked");
+    });
 });
 container.appendChild(pages.home);
+buttons[0].classList.add("clicked");
 
